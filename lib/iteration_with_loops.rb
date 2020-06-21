2db6ba56fe7a97c8e26f747eaa3acb84acb4713f
Kyle Walters <kyle.p.walters@gmail.com>
2020-06-21 16:18:10 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,4 +5,12 @@ end
   while row_index < src.count do
     element_index = 0 
     while element_index < src[row_index].count 
-      if src[row_index][element_index]
+      if src[row_index][element_index].class == strings
+        strings << src[row_index][element_index]
+      end
+        element_index += 1 
+      end
+      row_index += 1 
+    end
+    strings
+      
